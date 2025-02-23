0bbf008f68b81b6e80a4db601c0d2bc4faa4e4c0
MightyMountain <Ramaagung735@gmail.com>
2025-02-23 16:23:30 +0800

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,3 +1,2 @@
 alert('Website ini dibuat dengan cinta❤️, Maksudnya dengan Coding.')
-alert('Note: Tolong rahasiakan identitas "AUTHOR KEJAM" yang sebenarnya')
 alert('Nikamati Novel dari Author, secara gratis. Original buatan sendiri. Genre kebanyakan dari Novel, adalah genre Romance Tragis, Supernatural Tragic Romance, Sadnees, dan teman-temannya.')
