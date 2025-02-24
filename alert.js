51242ac9709e0cd7bf8bb79a0b628fe808bd9669
MightyMountain <Ramaagung735@gmail.com>
2025-02-24 21:37:11 +0800

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,2 +1,3 @@
-alert('Website ini dibuat dengan cinta❤️, Maksudnya dengan Coding.')
-alert('Nikamati Novel dari Author, secara gratis. Original buatan sendiri. Genre kebanyakan dari Novel, adalah genre Romance Tragis, Supernatural Tragic Romance, Sadnees, dan teman-temannya.')
+alert('Website ini dibuat dengan cinta❤️ Maksudnya dengan Coding.')
+
+alert('Note: Website masih dalam tahap pengembangan dan kemungkinan hanya berjalan di prangkat Desktop. Jika susunan UI tidak rapi gunakan mode Desktop untuk ponsel. Atau jika berkenan, gunakan rotasi layar Landscape pada ponsel anda')
