6581409df71e5e692a47bc8549cdf8c32d9b09c7
MightyMountain <Ramaagung735@gmail.com>
2025-07-18 18:54:51 +0800

    Update alert.js
    delleted 1 alert

--- alert.js
+++ alert.js
@@ -1,3 +1,1 @@
 alert('Website ini dibuat dengan cinta❤️ Maksudnya dengan Coding.')
-
-<!--!alert('Note: Website masih dalam tahap pengembangan dan kemungkinan hanya berjalan di prangkat Desktop. Jika susunan UI tidak rapi gunakan mode Desktop untuk ponsel.')-->
